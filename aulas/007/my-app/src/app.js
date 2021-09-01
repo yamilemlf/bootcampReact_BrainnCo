301bb4601cc43659056e60d93ecf4e0decc3ab5f
yamilemlf <yamilemlessa@gmail.com>
2021-09-01 16:59:06 -0300

    aula 7

---
+++ aulas/007/my-app/src/app.js
@@ -0,0 +1,35 @@
+function App () {
+    return (
+        <div>
+            <Title>Título</Title>
+            <Sidebar />
+            <Content />
+        </div>
+        
+    )
+}
+
+function Title ({ children }) {
+    return (
+    <h1>{children}</h1>
+    );
+};
+
+function Sidebar () {
+    return (
+        <aside>
+            <h2>Sidebar</h2>
+        </aside>
+    );
+};
+
+function Content () {
+    return (
+        <section>
+            <h2>Conteúdo</h2>
+            <p>Parágrafo</p>
+        </section>
+    )
+}
+
+export default App;
