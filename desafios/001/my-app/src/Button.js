a9726fc5bddba8e4c720d706ff11b87244036cf0
yamilemlf <yamilemlessa@gmail.com>
2021-09-02 14:58:16 -0300

    Desf 1 corrigindo button

--- desafios/001/my-app/src/Button.js
+++ desafios/001/my-app/src/Button.js
@@ -1,6 +1,6 @@
 function Button (props) {
     return (
-        <button kind={props.kind} class={props.kind}>
+        <button class={props.kind}>
             {props.children}
         </button>
     );
