dce32e15aa4aa669206c71a81129d0d3cd8dc8cb
yamilemlf <yamilemlessa@gmail.com>
2021-09-02 16:31:52 -0300

    aula 9

---
+++ aulas/009/my-app/src/app.js
@@ -0,0 +1,10 @@
+function App () {
+    return (
+        <button onClick={(e) => alert('clicou!')}>Clique aqui</button>
+    )
+    //Ou
+    //function handleClick() {alert('clicou!')};
+    //<button onClick={handleClick}>Clique aqui</button>
+}
+
+export default App;
